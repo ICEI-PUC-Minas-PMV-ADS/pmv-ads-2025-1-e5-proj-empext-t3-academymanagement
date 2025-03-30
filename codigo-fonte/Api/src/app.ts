7ea34748e5cccfface47f22516fc47e53d786a3d
Alameda-Indiano <thiago2332003@gmail.com>
2025-03-30 18:02:20 -0300

    fix: Config Cors

--- codigo-fonte/Api/src/app.ts
+++ codigo-fonte/Api/src/app.ts
@@ -1,10 +1,17 @@
+import cors from 'cors';
 import express from 'express';
 
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 
 const app = express();
+
 app.use(express.json());
+app.use(cors({
+	origin: '*',
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
--- codigo-fonte/Api/src/server.ts
+++ codigo-fonte/Api/src/server.ts
@@ -1,13 +1,6 @@
-import cors from 'cors';
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
-
-app.use(cors({
-	origin: '*',
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
