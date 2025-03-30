2ff47941f1ac5afdf20821d4f991dd3eeee28c72
Alameda-Indiano <thiago2332003@gmail.com>
2025-03-30 17:53:48 -0300

    fix: Config Cors

--- codigo-fonte/Api/src/server.ts
+++ codigo-fonte/Api/src/server.ts
@@ -1,6 +1,12 @@
+import cors from 'cors';
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	credentials: true,
+}));
 
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
