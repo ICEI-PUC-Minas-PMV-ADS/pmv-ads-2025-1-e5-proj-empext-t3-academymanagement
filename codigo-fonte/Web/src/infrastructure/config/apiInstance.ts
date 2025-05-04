e26974ccde46af58e5c121fa16e920d48a6dd465
Alameda-Indiano <thiago2332003@gmail.com>
2025-05-04 17:31:11 -0300

    fix: api instance

--- codigo-fonte/Web/src/infrastructure/config/apiInstance.ts
+++ codigo-fonte/Web/src/infrastructure/config/apiInstance.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const apiInstance: AxiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_HOST_ADMIN_API,
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
